9f2a1ea770a94232a85c79fbca077fe8e70d319e
carolina.mantilla14@gmail.com <carolina.mantilla14@gmail.com>
2021-10-10 21:58:44 -0500

    Crud with VerifyProducts

--- routes/sales.router.js
+++ routes/sales.router.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const {salesController} = require('../controllers');
-const verifyProducts = require('../mdl/VerifyProducts')
+const verifyProducts = require('../mdl/VerifyProducts');
 
 router.get('/uno', salesController.getSale);
 
