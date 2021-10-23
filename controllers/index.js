fff5c649c8810b4e8932033f7aa92546d4e41c66
Andrea Zorro <andreazrro@gmail.com>
2021-10-23 15:23:42 -0500

    Backend de crud usuarios Parte Andrea

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    usersController: require('./users.controller'),
+    authController: require('./auth.controller')
+}
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    usersRouter: require('./users.router'),
+    authRouter: require('./auth.router')
+}
